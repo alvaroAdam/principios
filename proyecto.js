17fe91d732f585a42a5cd164968b878bc047dced
Alvaro <alvaroandroid1@gmail.com>
2023-02-06 12:12:36 +0100

    agregar proyecto.js

---
+++ proyecto.js
@@ -0,0 +1,112 @@
+// function crearVector(n,m){
+    
+//     let ar=[];
+//     for(i=0;i<m;i++){
+//          ar.push(Math.round(Math.random() * n) )       
+         
+
+//     }
+    
+//     console.log(ar)
+// }
+
+// crearVector(6,8)
+
+
+// function sumarVector(v1,v2){
+
+// let d;
+// let c;
+// let res=[];
+// for (i=0;i<v1.length;i++){
+//     c=v1.length
+//    }
+
+//    for (i=0;i<v2.length;i++){
+//     d=v2.length
+//    }
+
+// if (c==d){
+//     for(i=0;i<v1.length;i++){
+//         res[i]=v1[i]+v2[i]
+//     }
+// }
+// console.log(res)
+
+// }
+// sumarVector([1,2,3,4] ,[1,2,3,4])
+
+
+
+// function productoNumeroVector(n,v)
+// {
+//     let res=[];
+//     for(i=0;i<v.length;i++){
+//            res[i]=v[i] * n
+// }
+// console.log(res)
+
+// }
+
+// productoNumeroVector(2,[1,2,3])
+
+
+// function restaVector(v1, v2)
+// {
+//     let d;
+//     let c;
+//     let res=[];
+//     for (i=0;i<v1.length;i++){
+//         c=v1.length
+//        }
+       
+    
+//        for (i=0;i<v2.length;i++){
+//         d=v2.length
+//        }
+    
+//     if (c==d){
+//         for(i=0;i<v1.length;i++){
+//             res[i]=v1[i]-v2[i]
+//         }
+//     }
+//     console.log(res)
+    
+//     }
+//     restaVector([3,4,5,6] ,[1,2,3,4])
+
+//     function productoVector(v1, v2)
+// {
+//     let d;
+//     let c;
+//     let res=[];
+//     for (i=0;i<v1.length;i++){
+//         c=v1.length
+//        }
+       
+    
+//        for (i=0;i<v2.length;i++){
+//         d=v2.length
+//        }
+    
+//     if (c==d){
+//         for(i=0;i<v1.length;i++){
+//             res[i]=v1[i]* v2[i]
+//         }
+//     }
+//     console.log(res)
+    
+//     }
+//     productoVector([3,4,5,6] ,[1,2,3,4])
+     
+     
+function cuadrado(v1)
+{
+    let d;
+    d=v1*v1
+    
+    console.log(d)
+    
+    }
+    cuadrado(3)
+     
